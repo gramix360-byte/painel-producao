c490f652e21646972623c685fce629cab73cea7a
gramix360-byte <gramix360@gmail.com>
2026-08-22 22:41:28 -0300

    Atualizar cache para versão com login

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='painel-producao-v2';
-const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./producao.html','./producao.js'];
+const CACHE='painel-producao-v3';
+const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./logo.svg','./producao.html','./producao.js','./login.html','./login.css','./login.js','./auth-client.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
