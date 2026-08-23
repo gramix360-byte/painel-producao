1fcfe37fd981f301dc4d9ab8073ef6b517cb0fe8
gramix360-byte <gramix360@gmail.com>
2026-08-22 23:50:25 -0300

    Atualizar cache para versão mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='painel-producao-v6';
+const CACHE='painel-producao-v7';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./logo.svg','./producao.html','./producao.js','./login.html','./login.css','./login.js','./auth-client.js','./production-launcher.js','./print-helper.js','./ficha.html','./ficha.css','./ficha.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
